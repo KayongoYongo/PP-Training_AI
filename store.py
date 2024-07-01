d7f27f65da16aae7be6e86f3d7ae330b0735dbd6
KayongoYongo <sknsammie10@gmail.com>
2024-07-01 23:42:45 +0300

    only works with OPEN AI

--- store.py
+++ store.py
@@ -1,36 +1,34 @@
 from PyPDF2 import PdfReader
-from langchain_community.embeddings import OpenAIEmbeddings
+from langchain.embeddings.openai import OpenAIEmbeddings
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.vectorstores import Chroma
-from langchain.document_loaders import PDFLoader
 
 # Path to the PDF document
-pdf_path = "pdf/Investments.pdf"
+pdf_reader = PdfReader("pdf/Investments.pdf")
 
-# Open the PDF file in read-binary mode
-with open(pdf_path, "rb") as file:
-    reader = PdfReader(file)
-    pdf_text = ""
+# print(pdf_reader)
+pdf_text = ""
 
-    # Iterate through all the pages of the PDF and extract text
-    for page_num in range(len(reader.pages)):
-        page = reader.pages[page_num]
-        pdf_text += page.extract_text()
+# Iterate through all the pages of the PDF and extract text
+for i, page in enumerate(pdf_reader.pages):
+    text = page.extract_text()
+    if text:
+        pdf_text += text
 
-# Initialize the loader with the extracted PDF text
-loader = PDFLoader(pdf_text)
-
-# Load the documents from the PDF text
-docs = loader.load()
-
+# print(len(pdf_text))
 # Initialize a text splitter with a chunk size of 1000 characters and an overlap of 200 characters
-text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
+text_splitter = CharacterTextSplitter(
+    separator = "\n",
+    chunk_size=1000, 
+    chunk_overlap=200,
+    length_function=len
+)
 
 # Split the loaded documents into smaller chunks
-splits = text_splitter.split_documents(docs)
+documents = text_splitter.split_text(pdf_text)
 
 # Create a vector store from the split documents using OpenAI embeddings
 # and persist the vector store in the specified directory
-Chroma.from_documents(documents=splits,
+Chroma.from_documents(documents=documents,
                       embedding=OpenAIEmbeddings(),
                       persist_directory="./chroma_db")
